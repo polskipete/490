f0ca7cb89cd8ffe438dab29e86767b19503f148d
Polskipete <pete885@gmail.com>
2018-10-11 17:40:39 -0400

    Extracting Data from mysportsfeeds

---
+++ playerStatsAPI.php
@@ -0,0 +1,39 @@
+#!/usr/bin/php
+<?php
+
+// Get cURL resource
+$ch = curl_init();
+
+// Set url
+curl_setopt($ch, CURLOPT_URL, 'https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2016-regular/cumulative_player_stats.json?team=bos');
+
+// Set method
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+// Set options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Set compression
+curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+
+// Set headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+	"Authorization: Basic " . base64_encode( '4cba762b-6615-4c0e-a99f-ccc429' . ":" . 'njitmysportsfeeds')
+]);
+
+// Send the request & save response to $resp
+$resp = curl_exec($ch);
+
+if (!$resp) {
+	die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+} else {
+	echo "Response HTTP Status Code : " . curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	echo "\nResponse HTTP Body : " . $resp;
+	//$json_data = json_encode($resp);
+	file_put_contents('myfile.json', $resp);
+}
+
+// Close request to clear up some resources
+curl_close($ch);
+
+?>
