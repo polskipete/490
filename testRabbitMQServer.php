fc06430e958f51d947bd8f1b974afcdefb0e3c0d
marc-e3 <me236@njit.edu>
2018-10-16 11:54:04 -0400

    Managed to redirect the mysql error message to a file

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -9,14 +9,15 @@ function doLogin($user,$pass)
 	//Db login Variables
         $servername = "localhost";
         $username = "dbAdmin";
-        $password = "password123!";
+        $password = "password123";
 	$dbname = "loginDB";
 	//Try to connect to db and store the results in conn
         $conn = mysqli_connect($servername, $username, $password, $dbname);
 	//Comment if DB login was succesful or not
         if(!$conn)
         {
-                printf("DB Connection Failed: ".mysqli_connect_error());
+		$error = "DB Connection Failed: ".mysqli_connect_error();
+		file_put_contents('Errorlog.txt', $error, FILE_APPEND);
 	}
 	else
 	{
