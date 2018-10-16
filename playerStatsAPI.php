ca416f7932bbe3ca158d2b382daef0c16c4daa13
Polskipete <pete885@gmail.com>
2018-10-15 23:48:32 -0400

    Pulling player stats and info in seperate variables

--- playerStatsAPI.php
+++ playerStatsAPI.php
@@ -8,7 +8,7 @@ foreach($teams as $team){
 	echo $team;
 	echo "\n";
 
-	//probably will use for error logging, to make sure each team gets through
+	//probably will use for error logging, to make sure each team gets thro
 	$count++;
 }
 //check if all 30 teams get through
@@ -17,7 +17,7 @@ echo $count;
 $ch = curl_init();
 
 // Set url
-curl_setopt($ch, CURLOPT_URL, 'https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/cumulative_player_stats.json?team=okl');
+curl_setopt($ch, CURLOPT_URL, 'https://api.mysportsfeeds.com/v2.0/pull/nba/2017-2018-regular/player_stats_totals.json?team=det');
 
 // Set method
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -30,7 +30,7 @@ curl_setopt($ch, CURLOPT_ENCODING, "gzip");
 
 // Set headers
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-	"Authorization: Basic " . base64_encode( '4cba762b-6615-4c0e-a99f-ccc429' . ":" . 'njitmysportsfeeds')
+	"Authorization: Basic " . base64_encode( '4cba762b-6615-4c0e-a99f-ccc429' . ":" . 'MYSPORTSFEEDS')
 ]);
 
 // Send the request & save response to $resp
@@ -39,20 +39,59 @@ $resp = curl_exec($ch);
 if (!$resp) {
 	die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
 } else {
-	//echo "Response HTTP Status Code : " . curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	echo "Response HTTP Status Code : " . curl_getinfo($ch, CURLINFO_HTTP_CODE) . "\n";
 	//echo "\nResponse HTTP Body : " . $resp;
-	file_put_contents('myfile.json', $resp);
+	file_put_contents('myfile.txt', $resp);
 	$obj = json_decode($resp, true);
-	//Gets exact player stats from team
-	var_dump($obj['cumulativeplayerstats']['playerstatsentry'][0]);
-	//find out array length of 
-	foreach ($obj as $player){
-		//var_dump($player);
-		$count++;
-	}
+	$playerCount = 0;
 	
+	foreach ($obj["playerStatsTotals"] as $player){
+		// all data for one player
+		var_dump($obj["playerStatsTotals"][$playerCount]);
+		// Player Info
+
+		// Personal
+		$playerID = $obj["playerStatsTotals"][$playerCount]["player"]["id"];
+		$last = $obj["playerStatsTotals"][$playerCount]["player"]["lastName"];
+		$first = $obj["playerStatsTotals"][$playerCount]["player"]["firstName"];
+
+		// Team
+		$teamID = $obj["playerStatsTotals"][$playerCount]["player"]["currentTeam"]["id"];
+		$team = $obj["playerStatsTotals"][$playerCount]["player"]["currentTeam"]["abbreviation"];
+ 
+		// Player stats
+
+		// field goals attempted
+		$feildGoals = $obj["playerStatsTotals"][$playerCount]["stats"]["fieldGoals"]["fgAtt"];
+		// free throws attempted
+		$freeThrowsAtt = $obj["playerStatsTotals"][$playerCount]["stats"]["freeThrows"]["ftAtt"];
+		// free throws made
+		$freeThrowsMade = $obj["playerStatsTotals"][$playerCount]["stats"]["freeThrows"]["ftMade"];
+		// Offensive rebounds
+		$offRebounds = $obj["playerStatsTotals"][$playerCount]["stats"]["rebounds"]["offReb"];
+		// defensive rebounds
+		$defRebounds = $obj["playerStatsTotals"][$playerCount]["stats"]["rebounds"]["defReb"];
+		// steals
+		$steals = $obj["playerStatsTotals"][$playerCount]["stats"]["defense"]["stl"];
+		// assists
+		$assists = $obj["playerStatsTotals"][$playerCount]["stats"]["offense"]["ast"];
+		// blocks
+		$blocks = $obj["playerStatsTotals"][$playerCount]["stats"]["defense"]["blk"];
+		// fouls
+		$fouls = $obj["playerStatsTotals"][$playerCount]["stats"]["miscellaneous"]["fouls"];
+		// turnover
+		$turnover = $obj["playerStatsTotals"][$playerCount]["stats"]["defense"]["tov"];
+		
+		// Test if data is stored properly on variable
+		echo $playerID . "\n" . $last . "\n" . $first . "\n" . $teamID . "\n" . $team . "\n" ;
+		echo $feildGoals . "\n" . $freeThrowsAtt . "\n" . $freeThrowsMade . "\n" . $offRebounds . "\n" . $defRebounds . "\n";
+		echo $steals . "\n" . $assists . "\n" . $blocks . "\n" . $fouls . "\n" . $turnover . "\n";
+		
+		$playerCount += 1; 
+	}
+echo "\n\n" . $playerCount;
 }
-echo $count;
+//echo $count;
 // Close request to clear up some resources
 curl_close($ch);
 
