65b0eab3c21224910a772f5142b3ae100f6050aa
marc-e3 <you@example.com>
2018-11-24 14:14:11 -0500

    new listener and directories made for the deployment system

---
+++ deployment/dServer.php
@@ -0,0 +1,62 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');	
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+function doLogin($user,$pass)
+{
+	//Db login Variables
+        $servername = "localhost";
+        $username = "dbAdmin";
+        $password = "password123!";
+	$dbname = "loginDB";
+	//Try to connect to db and store the results in conn
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+	//Comment if DB login was succesful or not
+        if(!$conn)
+        {
+                printf("DB Connection Failed: ".mysqli_connect_error());
+	}
+	else
+	{
+		// Now connected to database
+		printf("DB Connection Succesful! \n");
+		// Get data from database that equals the values recived from client
+		$result = mysqli_query($conn, "select * from loginTable where username = '$user' and password ='$pass'") or die("Failed to query".mysqli_error());
+		// Store data that was retrieved in an array
+		$row = mysqli_fetch_array($result);
+		// checks if login info is correct
+        	if($row['username'] == $user && $row['password'] == $pass)
+        	{
+                	printf ("Login successful! \n Welcome user ".$row['username']);
+                	return true;
+        	}
+        	//if it does not match return false 
+        	else
+       		{
+                	printf ('Login failed');
+                	return false;
+        	}
+  	}
+}
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "Login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+$server = new rabbitMQServer("RabbitMQ.ini","testServer");
+$server->process_requests('requestProcessor');
+exit();
+?>
