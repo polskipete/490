b014c618ad41e2e30bc38a74d42cb46036242109
root <ggg7@njit.edu>
2018-11-25 18:09:28 -0500

    insterting tar.gz into database with timestamp

--- deployment/dbCode.php
+++ deployment/dbCode.php
@@ -1,13 +1,22 @@
 #!/usr/bin/php
 <?php
+	include("dbconfig.php");
 	function chopFilename ($filename){
 		$round1 = substr ($filename, 0, strrpos($filename,'.'));
 		return $round2 = substr ($round1, 0, strrpos($round1,'.'));
+	}
+	function addVersion($conn, $version){
+		echo $version;
+		$time = date('Y-m-d G:i:s');
+		echo $time;
+		$sql = "INSERT INTO deploymentTable (version, location, date) VALUES 			('$version','deployment', '$time')";
+		$add = mysqli_query($conn, $sql);
 	}
 	
 	$filename = "1_0.tar.gz";
 
 	echo chopFilename($filename);
+	addVersion($conn, chopFilename($filename));
 
 	
 ?>
