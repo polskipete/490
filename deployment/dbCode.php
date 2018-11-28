caac183567ef19c94a5c8e1a44569b346cd6ef70
Polskipete <pete885@gmail.com>
2018-11-28 18:41:19 -0500

    Built the code to take incoming tar.gz file and storing it in the database and moved to patch file

--- deployment/dbCode.php
+++ deployment/dbCode.php
@@ -1,26 +1,51 @@
 #!/usr/bin/php
 <?php
 	include("dbconfig.php");
-	function chopFilename ($filename){
+	$filename = getFileName();
+	addVersion($conn, $filename);
+
+	function addVersion($conn, $filename){
+		$time = date('Y-m-d G:i:s');
+		$status = "Bad";
+		$versionSQL = "SELECT MAX(Version) FROM deploymentTable";
+	
+		$result = mysqli_query($conn, $versionSQL);
+		$countRow = mysqli_fetch_assoc($result);
+		var_dump($countRow);		
+		$max = intval($countRow['MAX(Version)']);
+		$max ++;
+		//echo $max;
+		$newFileName = createFileName($max, $filename);
+		$type = getFileType($filename);
+		moveFile($newFileName, $filename);
+
+		$sql = "INSERT INTO deploymentTable(Status, FileName, Type, Date)VALUES ('$status', '$newFileName', '$type', '$time')";
+		$result = mysqli_query($conn, $sql);
+		
+	}
+	function createFileName($version, $filename){
+		$newFile =  $version . "_" . $filename;
+		echo $newFile;
+		return $newFile;
+	}
+	function getFileName(){
+		$path = 'patches/transferingPatch';
+		$file = array_diff(scandir($path), array('.', '..'));
+		return $file[2];	
+	}
+	function getFileType($filename){
+		$fileType = chopFilename($filename);
+		return $fileType;
+	}
+	function chopFilename($filename){
 		$round1 = substr ($filename, 0, strrpos($filename,'.'));
 		return $round2 = substr ($round1, 0, strrpos($round1,'.'));
-	}
-	function addVersion($conn, $version){
-		echo $version;
-		$time = date('Y-m-d G:i:s');
-		echo $time;
-		$sql = "INSERT INTO deploymentTable (version, location, date) VALUES 			('$version','$version.tar.gz', '$time')";
-		$add = mysqli_query($conn, $sql);
-	}
-	
-	$filename = "1_0.tar.gz";
-	//mock server shit
-	$path = '../cunt.tar.gz';
-	$name = basename($path);
-	echo chopFilename($name);
-	addVersion($conn, chopFilename($name));
+	}	
+	function moveFile($newFileName, $filename){
+		$movePatch = "\n sudo mv patches/transferingPatch/$filename patches/$newFileName";
+		exec($movePatch);
 
-	
+	}
 ?>
 
 
--- deployment/deploymentDB.php
+++ deployment/deploymentDB.php
@@ -8,10 +8,10 @@
 	*/
 	/* Copy and paste when logged into dbAdmin
  	use deploymentDB;
-	CREATE TABLE deploymentTable(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, version 		VARCHAR(30) NOT NULL, location VARCHAR(30) NOT NULL, date TIMESTAMP);
+	CREATE TABLE deploymentTable(Status VARCHAR(30) NOT NULL, Version INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, FileName VARCHAR(30) NOT NULL, Type VARCHAR(30), Date TIMESTAMP);
 	SELECT * FROM deploymentTable;
  
-	insert into deploymentTable (version, location, date) VALUES ('1.0', 'deployment', 		'2017-07-03');
+	insert into deploymentTable (Status, FileName, Type, Date) VALUES ('Bad', '0.tar.gz', 'html', '2018-07-03');
 
  	*/
 ?>
