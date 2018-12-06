0f763fd22843b7832b0e8b42665cc5d8435f6ef8
root <ggg7@njit.edu>
2018-12-06 15:01:41 -0500

    systemd auto

--- deployment/dbCode.php
+++ deployment/dbCode.php
@@ -3,7 +3,6 @@
 	include("dbconfig.php");
 	$filename = getFileName();
 	addVersion($conn, $filename);
-
 	function addVersion($conn, $filename){
 		$time = date('Y-m-d G:i:s');
 		$status = "Bad";
@@ -18,7 +17,6 @@
 		$newFileName = createFileName($max, $filename);
 		$type = getFileType($filename);
 		moveFile($newFileName, $filename);
-
 		$sql = "INSERT INTO deploymentTable(Status, FileName, Type, Date)VALUES ('$status', '$newFileName', '$type', '$time')";
 		$result = mysqli_query($conn, $sql);
 		
@@ -31,6 +29,7 @@
 	function getFileName(){
 		$path = 'patches/transferingPatch';
 		$file = array_diff(scandir($path), array('.', '..'));
+		echo $file[2];
 		return $file[2];	
 	}
 	function getFileType($filename){
@@ -42,11 +41,7 @@
 		return $round2 = substr ($round1, 0, strrpos($round1,'.'));
 	}	
 	function moveFile($newFileName, $filename){
-		$movePatch = "\n sudo mv patches/transferingPatch/$filename patches/$newFileName";
+		$movePatch = "\n sudo mv /var/www/html/490/deployment/patches/transferingPatch/$filename /var/www/html/490/deployment/patches/$newFileName";
 		exec($movePatch);
-
 	}
 ?>
-
-
-
---
+++ patchTransfer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+	/usr/bin/php /var/www/html/490/patchTransfer.sh
+	sleep 15
+done
