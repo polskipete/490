7856b91617bb546375f9139ff0f25ce5932508d0
Jermaynen <gkelectronics139@gmail.com>
2018-12-13 14:26:16 -0500

    Added pages

--- phpFE/main.php
+++ phpFE/main.php
@@ -84,12 +84,22 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   <hr>
   <div class="w3-bar-block">
     <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding w3-blue" onclick="team_close(); match_close(); main_open()"><i class="fa fa-  fa-fw"></i>Home Page</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding" onclick="main_close(); team_close(); match_open()"><i class=" "></i>    Match Making</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding" onclick="main_close(); match_close(); team_open()"><i class=" "></i>    Team Making</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding w3-blue" onclick="team_close(); match_close(); history_close(); list_close(); tutorial_close(); main_open()"><i class="fa fa-  fa-fw"></i>Home Page</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding" onclick="main_close(); match_close(); tutorial_close(); history_close(); list_close(); team_open()"><i class=" "></i>    Team Making</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding" onclick="main_close(); team_close(); tutorial_close(); history_close(); list_close(); match_open()"><i class=" "></i>    Match Making</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding" onclick="main_close(); team_close(); tutorial_close(); match_close(); list_close(); history_open();"><i class=" "></i>    History</a>
+
     <a href="#" class="w3-bar-item w3-button w3-padding w3-red"><i class=" "></i>    Log Out</a>
-    <!--<a href="#" class="w3-bar-item w3-button w3-padding"><i class=" "></i>    History</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class=" "></i>    Setting</a>-->
+
+    <a href="#" class="w3-bar-item w3-button w3-padding w3-Pale-Blue" onclick="main_close(); match_close(); team_close(); history_close(); list_close(); tutorial_open()"><i class=" "></i> Need Help? Click for Tutorial </a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding w3-light-grey" onclick="main_close(); match_close(); team_close(); history_close(); tutorial_close(); list_open();"><i class=" "></i> Don't know what players are in the league? <br> 	Click for full list </a>
+
+    <!--<a href="#" class="w3-bar-item w3-button w3-padding"><i class=" "></i>    Setting</a>-->
     <br><br>
   </div>
 </nav>
@@ -149,6 +159,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
     </div>
   </div>
 
+  <!-- !CURRENT TEAM SECTION! -->
   <div class="w3-panel">
     <div class="w3-row-padding" style="margin:0 -16px">
       <div class="w3-container">
@@ -190,7 +201,8 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   </div>
   
   <br>
-
+  
+   
   <!-- Footer -->
   <!--<footer class="w3-container w3-padding-6 w3-black">
     <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
@@ -313,7 +325,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 		         </select>
 		  </label>
 	    </div>
-
+		<br>
 	    <div>
 		<button class="pure-button pure-button-primary" type="submit" name="submit" id="submit">Submit</button>
 	    </div>
@@ -321,7 +333,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
 </div>
 
-<!-- -------------------------------------------------------------------------------------------------------------------Match Making Page
+<!-- -------------------------------------------------------------------------------------------------------------------Match Making Page-->
 <!-- !PAGE MATCH MAKING CONTENT! -->
 <div class="w3-main" style="margin-left:300px;margin-top:43px;" id="myMatchMaking">
   <!-- Header -->
@@ -329,7 +341,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
     <h5><b><i class="fa fa-dashboard"></i> My Match Making</b></h5>
   </header> 
          
-  <form  class="form-inline" method="POST" action="matchmaking.php">
+  <form  class="form-inline" method="POST" action="../phpBE/matchmaking.php">
 
   <div class="w3-container">
        <h3> When you hit start match below you will be randomly paired up with another team and you will vs them </h3>
@@ -338,7 +350,90 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
        <button class="btn btn-lg btn-primary btn-block btn-login" type="submit" ="submit" id="submit">Start Match</button>
   </div>
 </div>
+<!-------------------------------------------------------------------------------------------------------------------History-->
+<!-- !HISTORY! -->
+<div class="w3-main" style="margin-left:300px;margin-top:43px;" id="myHistory">
+  <!-- !MATCH HISTORY PAGE! -->
+  <div class="w3-panel">
+  <header class="w3-container" style="padding-top:22px">
+    <h5><b><i class="fa fa-dashboard"></i> My History</b></h5>
+  </header> 
+      <div class="w3-container">
+        <table class="w3-table w3-striped w3-white">
+          <tr>
+            <td><?phpecho $_SESSION["result"];?></td>
+	    <td><?phpecho $_SESSION[""];?></td>
+	    <td><?phpecho $_SESSION["player1"];?></td>
+	    <td><?phpecho $_SESSION["player1"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player2"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player3"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player4"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player5"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player6"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player7"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player8"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player9"];?></td>
+          </tr>
+          <tr>
+            <td><?phpecho $_SESSION["player10"];?></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+</div>
 
+
+<!-------------------------------------------------------------------------------------------------------------------Tutorial Page-->
+<!-- !TUTORIAL PAGE! -->
+<div class="w3-main" style="margin-left:300px;margin-top:43px;" id="myTutorial">
+  <!-- Header -->
+  <header class="w3-container" style="padding-top:22px">
+    <h5><b><i class="fa fa-dashboard"></i> My Tutorial Section</b></h5>
+  </header> 
+ 
+  <div class="w3-container">
+       <h2>
+	          1. You’ve been gifted with 1,000 coins, please go purchase your team on the Team Making Tab.
+	     <br> 2. Once you create your team and go back to the home page you will see your team and efficency displayed. 
+	     <br> 3. At the top of the home page tab then you will see you current count for wins, losses, and draws. 
+	     <br> 4. If you go to the matchmaking page to verse another team. Your new results will be displayed. 
+	     <br> 5. To log out just hit the red logout button on the main screen.
+	     <br> 6. We hope you enjoyed our fantasy web app, thank you. </h2>
+</div>
+</div>
+
+<!-------------------------------------------------------------------------------------------------------------------NBA Player List-->
+<!-- !List PAGE! -->
+<div class="w3-main" style="margin-left:300px;margin-top:43px;" id="myList">
+  <!-- Header -->
+  <header class="w3-container" style="padding-top:22px">
+    <h5><b><i class="fa fa-dashboard"></i> NBA Players</b></h5>
+  </header> 
+ 
+  <div class="w3-container">
+       <h2>
+	    NBA TEAM LIST HERE
+				 </h2>
+</div>
+</div>
+
+<!-------------------------------------------------------------------------------------------------------------------Scripts-->
 
 <script>
 	// Get the Sidebar and MainContent
@@ -346,6 +441,11 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 	var myMainContent = document.getElementById("myMainContent");
 	var myMatchMaking = document.getElementById("myMatchMaking");
 	var myTeamMaking = document.getElementById("myTeamMaking");
+	var myHistory = document.getElementById("myHistory");
+	var myTutorial = document.getElementById("myTutorial");
+	var myList = document.getElementById("myList");
+
+
 
 	// Get the DIV with overlay effect
 	var overlayBg = document.getElementById("myOverlay");
@@ -378,6 +478,21 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 	function team_open() {
 	    myTeamMaking.style.display = 'block';
 	}
+
+	// Toggle between showing and hiding the History
+	function history_open() {
+	    myHistory.style.display = 'block';
+	}
+
+	// Toggle between showing and hiding the Tutorial
+	function tutorial_open() {
+	    myTutorial.style.display = 'block';
+	}
+	
+	// Toggle between showing and hiding the Tutorial
+	function list_open() {
+	    myList.style.display = 'block';
+	}
 	
         //------------------------------------------------------------------------------------------------
 
@@ -392,14 +507,29 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 	    myMainContent.style.display = "none";
 	}
 
-        // Close the MainContent with the close button
+        // Close the MatchMaking with the close button
 	function match_close() {
 	    myMatchMaking.style.display = "none";
 	}
 
-	// Close the MainContent with the close button
+	// Close the TeamMaking with the close button
 	function team_close() {
 	    myTeamMaking.style.display = "none";
+	}
+
+	// Close the TeamMaking with the close button
+	function history_close() {
+	    myHistory.style.display = "none";
+	}
+
+	// Close the Tutorial with the close button
+	function tutorial_close() {
+	    myTutorial.style.display = "none";
+	}
+	
+	// Close the Tutorial with the close button
+	function list_close() {
+	    myList.style.display = "none";
 	}
 </script>
 </body>
